12f1ae995ad08dfaaf95d40a02253b2e5732b232
clara leal <mariiaclaraleal@gmail.com>
2024-03-14 11:42:30 -0300

    adicionei o setor.h e setor.c, e comecei a struct do setor.c

---
+++ setor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "setor.h"
+
+typedef struct setor{
+    char nome[50];
+    char descrição[100];
+    char produto[50]
+}Setor;
+
+Setor *cria_setor(char nome, char descrição, char produto){
+    Setor* setor = (Setor*)malloc(sizeof(Setor*));
+
+    if (setor == NULL){
+        printf("ERRO NA ALOCAÇÃO DE MEMÓRIA!!!\n");
+        exit(1);
+    }
+
+
+
+    printf("Informe o nome do setor que deseja cadastrar:\n");
+    scanf("%s\n", setor->nome);
+}
